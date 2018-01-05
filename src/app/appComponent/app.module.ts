f404618575c8cbd1e477cb9dd239295a8ac10007
Levi Smith <smith.levidev@gmail.com>
2018-01-05 07:47:24 -0700

    initial commit

---
+++ src/app/appComponent/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "../material.module";
+
+
+import { AppComponent } from './app.component';
+import { CharacterListComponent } from '../characterList/character-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
